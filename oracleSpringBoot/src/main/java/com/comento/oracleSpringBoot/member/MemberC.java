0e19c253a987bd3a756a86b73f78069423fd8650
powerfulh <68536042+powerfulh@users.noreply.github.com>
2022-03-12 00:12:14 +0900

    롬복 추가

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/member/MemberC.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/member/MemberC.java
@@ -1,7 +1,6 @@
 package com.comento.oracleSpringBoot.member;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/member/entity/LoginVo.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/member/entity/LoginVo.java
@@ -2,20 +2,12 @@ package com.comento.oracleSpringBoot.member.entity;
 
 import javax.validation.constraints.NotBlank;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class LoginVo {
 	@NotBlank(message = "id require")
-	private String id;
+	String id;
 	String pw;
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getPw() {
-		return pw;
-	}
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
 }
