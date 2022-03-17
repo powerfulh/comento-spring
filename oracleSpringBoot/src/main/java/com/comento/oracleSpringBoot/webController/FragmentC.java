5f09ed1005292b43eed246ecf77087b2c8c195cd
hanhoon12 <hanhoon12@naver.com>
2022-03-17 14:25:33 +0900

    thymeleaf fragment controller test

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/webController/FragmentC.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/webController/FragmentC.java
@@ -1,13 +1,14 @@
 package com.comento.oracleSpringBoot.webController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Test {
+public class FragmentC {
 	@GetMapping("test")
-	public String test() {
-		
-		return "index";
+	public String test(Model m) {
+		m.addAttribute("test", "call");
+		return "fragment/frag :: fragnment";
 	}
 }
