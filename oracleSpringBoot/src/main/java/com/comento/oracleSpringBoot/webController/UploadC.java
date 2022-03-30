6a2122552f42b2011c62de31c0fc3ffafd10164e
powerfulh <68536042+powerfulh@users.noreply.github.com>
2022-03-30 16:45:28 +0900

    이제 서버에서 파일을 받을수 있다

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/webController/UploadC.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/webController/UploadC.java
@@ -1,8 +1,11 @@
 package com.comento.oracleSpringBoot.webController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("upload")
@@ -11,5 +14,11 @@ public class UploadC extends WebC{
 	public String index() {
 		
 		return "upload";
+	}
+	@PostMapping("proc")
+	public String upload(Model m, MultipartFile[] fileList) {
+		m.addAttribute("msg", "Upload fail");
+		System.out.println(fileList.length);
+		return "index";
 	}
 }
