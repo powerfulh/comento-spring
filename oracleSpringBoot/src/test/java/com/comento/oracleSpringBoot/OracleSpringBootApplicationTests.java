9413685515e23d9cd09f6e3ca189c6d601b547cc
powerfulh <68536042+powerfulh@users.noreply.github.com>
2022-03-31 18:52:06 +0900

    테스트코드 샘플

--- oracleSpringBoot/src/test/java/com/comento/oracleSpringBoot/OracleSpringBootApplicationTests.java
+++ oracleSpringBoot/src/test/java/com/comento/oracleSpringBoot/OracleSpringBootApplicationTests.java
@@ -1,13 +1,21 @@
 package com.comento.oracleSpringBoot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.comento.oracleSpringBoot.member.MemberS;
+import com.comento.oracleSpringBoot.member.entity.LoginVo;
 
 @SpringBootTest
 class OracleSpringBootApplicationTests {
-
+	@Autowired
+	MemberS service;
 	@Test
-	void contextLoads() {
+	void test() {
+		LoginVo lvo = new LoginVo();
+		lvo.setId("hh");
+		lvo.setPw("123");
+		System.out.println("select count from member : " + service.logicProc(lvo));
 	}
-
 }
