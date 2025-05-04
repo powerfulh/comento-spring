bfb07febc6af0facc94dc88b0160be78f3a23be1
powerfulh <68536042+powerfulh@users.noreply.github.com>
2025-05-04 21:51:26 +0900

    Devel server

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/RestExceptionReturn.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/RestExceptionReturn.java
@@ -1,13 +1,16 @@
 package com.comento.oracleSpringBoot;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice("com.comento.oracleSpringBoot.rest")
 public class RestExceptionReturn {
 	@ExceptionHandler
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public String exception(BindException e) {
 		return e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
 	}
