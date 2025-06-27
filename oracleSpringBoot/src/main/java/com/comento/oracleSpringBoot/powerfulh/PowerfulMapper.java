b6d529d58bd9999ae9e18d09690306ceb440b5ab
powerfulh <68536042+powerfulh@users.noreply.github.com>
2025-06-27 22:51:06 +0900

    Delete unused

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/powerfulh/PowerfulMapper.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/powerfulh/PowerfulMapper.java
@@ -1,20 +1,8 @@
 package com.comento.oracleSpringBoot.powerfulh;
 
-import java.util.List;
-import java.util.Map;
-
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-import com.comento.oracleSpringBoot.dto.medicine.Count;
 
 @Mapper
-@Repository
 public interface PowerfulMapper {
-	public void newMedicine(Map<String, String> param);
-	public List<Map<String, Object>> getMedicineList();
-	public int editMedicine(Map<String, String> param);
-	public int deleteMedicine(Map<String, String> param);
-	public List<Map<String, Object>> getMedicineListMostPlus();
-	public List<Count> selectMedicineCnt();
+	
 }
