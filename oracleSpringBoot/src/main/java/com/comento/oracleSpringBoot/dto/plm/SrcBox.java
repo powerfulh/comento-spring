05d9a439ab6f882bc27298aef467e1ac97feda52
powerfulh <68536042+powerfulh@users.noreply.github.com>
2025-11-29 21:00:51 +0900

    이슈 #36

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/dto/plm/SrcBox.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/dto/plm/SrcBox.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Size;
 
 public class SrcBox {
     @NotBlank
-    @Size(min = 1, max = 40)
+//    @Size(min = 1, max = 40) 입력 기준
+    @Size(min = 1, max = 60) // 출력 기준
     public String src;
 }
