7e107049f1e1249865685cd699ca75fe68ec5a18
powerful-room2 <hanhoon12@naver.com>
2026-09-13 23:14:22 +0900

    이렇게 했더니만 '은날씨가좋아서오랜만' 기준 문장 수 42개 나오던게 6천개가 나오고 지랄이다 시팔 내가 만든 건데 동작이 전혀 기억 안난다 내일 다시 한번 파헤쳐보자

--- oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/plm/StaticUtilFromModel.java
+++ oracleSpringBoot/src/main/java/com/comento/oracleSpringBoot/plm/StaticUtilFromModel.java
@@ -11,13 +11,24 @@ import java.util.stream.Collectors;
 
 public class StaticUtilFromModel {
     static final int opener = 2903;
+    public static int dc = 0;
 
     public static Predicate<Twoken> getContextFinder(int lw, int rw) {
         return item -> item.getLeftword() == lw && item.getRightword() == rw;
     }
-
+    static Toke generateToke(UnderstandTarget src, Word item, List<Toke> understandList, ContextCore contextCore, Toke lastUnderstand, List<Context> contextList, List<Compound> compoundList, final Dict wordList, boolean shouldSpace) {
+        Toke toke = src.getAvailableToke(item, shouldSpace);
+        if(toke == null || understandList.isEmpty()) return toke;
+        try {
+            contextCore.rightContext(toke, lastUnderstand, toke, contextList, compoundList, wordList, lastUnderstand.isRightSpace(), lastUnderstand.otherOption, 0);
+        } catch (PlmException e) {
+            return null;
+        }
+        return contextCore.step2(toke, lastUnderstand.isRightSpace()); // 이 부분만 모델이랑 울트론이 다르다. 모델과 동일한 동작이며 울트론도 따라와야 되지 않나 싶다
+    }
     // fix space api 를 위해 `spaceMap` 인자 추가
     public static void separateToken(List<Toke> understandList, UnderstandTarget src, final Dict wordList, Map<String, List<Word>> failHistory, List<Context> contextList, List<Sentence> sentenceList, List<Compound> compoundList, SuccessHistory successHistory, ContextCore contextCore, Map<Integer, Boolean> spaceMap) {
+        dc++;
         if(src.success()) sentenceList.add(new Sentence(understandList, contextList));
         else {
             Toke lastUnderstand = understandList.get(understandList.size() - 1);
@@ -34,17 +45,8 @@ public class StaticUtilFromModel {
             List<Word> h = failHistory.get(src.getRight());
             final List<Word> page = wordList.book.get(src.getRight().charAt(0));
             List<Toke> sameList = page == null ? Collections.emptyList() : page.stream()
-                    .map(item -> {
-                        final boolean shouldSpace = spaceMap.getOrDefault(item.getN(), false);
-                        Toke toke = src.getAvailableToke(item, shouldSpace);
-                        if(toke == null || understandList.isEmpty()) return toke;
-                        try {
-                            contextCore.rightContext(toke, lastUnderstand, toke, contextList, compoundList, wordList, lastUnderstand.isRightSpace(), lastUnderstand.otherOption, 0);
-                        } catch (PlmException e) {
-                            return null;
-                        }
-                        return contextCore.step2(toke, lastUnderstand.isRightSpace()); // 이 부분만 모델이랑 울트론이 다르다. 모델과 동일한 동작이며 울트론도 따라와야 되지 않나 싶다
-                    })
+                    // fix space 흐름에서는 재사용이 필요하므로 함수로 분리했다
+                    .map(item -> generateToke(src, item, understandList, contextCore, lastUnderstand, contextList, compoundList, wordList, false))
                     .filter(item -> {
                         if(item != null) {
                             if(h == null) return true;
@@ -66,11 +68,22 @@ public class StaticUtilFromModel {
             final Toke best = sameList.get(sameList.size() - 1);
             int ss = sentenceList.size();
             if(sameList.size() > 1) {
-                sameList.subList(0, sameList.size() - 1).stream()
-                        .filter(item -> item.getRightContext() > 0)
+                final boolean shouldSpace = spaceMap.getOrDefault(lastUnderstand.getN(), false);
+                sameList.subList(0, sameList.size() - 1)
+//                        .filter(item -> item.getRightContext() > 0)
                         .forEach(item -> {
                             List<Toke> clone = new ArrayList<>(understandList);
-                            separateToken(clone, src.clone().pushToke(clone, item), wordList, failHistory, contextList, sentenceList, compoundList, successHistory, contextCore, spaceMap);
+                            if (item.getRightContext() > 0) {
+                                separateToken(clone, src.clone().pushToke(clone, item), wordList, failHistory, contextList, sentenceList, compoundList, successHistory, contextCore, spaceMap);
+                            } else if(shouldSpace) {
+                                UnderstandTarget rollbackSrcClone = src.clone();
+                                rollbackSrcClone.rollback(lastUnderstand);
+                                if(clone.size() == 1) clone.set(0, rollbackSrcClone.getAvailableToke(lastUnderstand.src, true));
+                                else {
+                                    clone.set(clone.size() - 1, generateToke(rollbackSrcClone, lastUnderstand.src, understandList.subList(0, understandList.size() - 1), contextCore, understandList.get(understandList.size() - 2), contextList, compoundList, wordList, true));
+                                }
+                                separateToken(clone, src.clone().pushToke(clone, item), wordList, failHistory, contextList, sentenceList, compoundList, successHistory, contextCore, spaceMap);
+                            }
                         });
                 if(best.getRightContext() < 1) best.otherOption = true;
             }
